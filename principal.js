4ad568c6bfa7e8f995852e03cdf7c18b47a7a583
vmarialuz <vmarialuzm@gmail.com>
2023-12-08 11:39:57 -0500

    feat: add function payment_user

--- principal.js
+++ principal.js
@@ -1,10 +1,35 @@
-const pagosRealizados = document.querySelector("#pagosVencidos");
+const pagosRealizados = document.querySelector("#pagosRealizados");
 const pagosVencidos = document.querySelector("#pagosVencidos");
 
 const token = localStorage.getItem("token.access")
 console.log(token)
 
 
+// Funcion para pagos realizados
+async function getPagosRealizados() {
+    const response = await fetch("http://127.0.0.1:8000/api/v2/payment_user/", {
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    });
+    const data = await response.json();
+    data.results.forEach((pagoRealizado) => {
+        pagosRealizados.innerHTML += renderPagosRealizados(pagoRealizado);
+    })
+}
+
+getPagosRealizados();
+
+function renderPagosRealizados(pago) {
+    return `
+        <p>${pago.id}</p>
+        <p>${pago.amount}</p>
+        <p>${pago.payment_date}</p>
+        <p>${pago.expiration_date}</p>
+        <p>${pago.user_id}</p>
+        <p>${pago.service_id}</p>
+    `;
+}
 
 
 // Funcion para pagos vencidos
