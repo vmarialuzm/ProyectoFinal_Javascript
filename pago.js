f599922b077974226b3f451a25cf733b4f9b0d5f
vmarialuz <vmarialuzm@gmail.com>
2023-12-09 10:41:17 -0500

    feat: add function pago

--- pago.js
+++ pago.js
@@ -1,7 +1,11 @@
+const form = document.querySelector("form")
+const fechaVencimiento = document.querySelector("#fechaVencimiento");
 const servicioPago = document.querySelector("#servicioPago");
-const token = localStorage.getItem("token.access")
+const montoPago = document.querySelector("#montoPago");
+const token = localStorage.getItem("token.access");
+const email = localStorage.getItem("email");
 
-// Funcion para llevar de opciones el select
+// Funcion para llenar de opciones el select
 async function getServices() {
     const response = await fetch("http://127.0.0.1:8000/api/v2/services/", {
         headers: {
@@ -18,6 +22,53 @@ getServices();
 
 function renderServicios(servicio) {
     return `
-        <option value="${servicio.name}">${servicio.name}</option>
+        <option value="${servicio.id}">${servicio.name}</option>
     `;
 }
+
+// Funcion para buscar el user_id del usuario logueado
+async function getUserId() {
+    try {
+        const response = await fetch("http://127.0.0.1:8000/usuarios/")
+        const data = await response.json();
+        const user = data.results.find((user) => user.email === email)
+        return user.id
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+
+// Funcion para añadir nuevo pago
+form.onsubmit = async function (event) {
+    event.preventDefault();
+    const userId = await getUserId();
+
+    const body = {
+        user_id : userId,
+        expiration_date : fechaVencimiento.value,
+        service_id : servicioPago.value,
+        amount : montoPago.value
+    };
+    console.log(body)
+    try {
+        const response = await fetch("http://127.0.0.1:8000/api/v2/payment_user/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            body: JSON.stringify(body)
+        });
+        Swal.fire({
+            text: "Pago creado exitosamente",
+            icon: "success"
+        });
+    } catch (error) {
+        Swal.fire({
+            text: error,
+            icon: "error"
+        });
+    }
+};
